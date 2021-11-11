aaa91608b7beed951f8f4e06c0c50d20fbeaf515
Nikolas Bedini <nikolasbedini04@gmail.com>
2021-11-11 11:41:29 +0100

    Aggiungere i file di progetto.

---
+++ AtletiASPMVC/Controllers/NationsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AtletiASPMVC.Models;
+
+namespace AtletiASPMVC.Controllers
+{
+    public class NationsController : Controller
+    {
+        // GET: Nations
+        public ActionResult Random()
+        {
+            //Interrogazione del model
+
+            using(OlympicsEntities model = new OlympicsEntities())
+            {
+
+                Random RandomGenerator = new Random();
+                int RandomValue = RandomGenerator.Next(model.Nationalities.Count() - 1);
+
+                Nationality nation = model.Nationalities
+                    .OrderBy(o => o.Id)
+                    .Skip(RandomValue)
+                    .FirstOrDefault();
+
+                return View(nation);
+            }
+        }
+    }
+}
